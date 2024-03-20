a092a167bab5bf1232e647e04e349c258831e238
i-saac-b <isbriggs539@gmail.com>
2024-03-20 16:16:07 -0400

    hotfix BuildingData guardrail
    prevent a possible mod crash on launch

--- obj/Debug/net6.0/PostBoxMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/PostBoxMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostBoxMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d96fd71a00c345b5591ec1c837976cf7db06169")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf3e63017981e9608a7e602fa795b9f0bfec751")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostBoxMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostBoxMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
